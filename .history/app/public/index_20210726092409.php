ef7812d8e4f671c4624c5c721d828c598a262bdc
Grzegorz Muszyński <grzemu@agora.pl>
2021-07-26 09:45:00 +0200

    przekonfigurowanie

---
+++ .history/app/public/index_20210726092409.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Simple Bookstore API
+ * @version 1.0.0
+ */
+
+require_once __DIR__ . './../vendor/autoload.php';
+
+$app = new Slim\App();
+
+
+/**
+ * POST addBook
+ * Summary: Add a new book to the store
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->POST('/book', function($request, $response, $args) {
+            
+            
+            
+            $body = $request->getParsedBody();
+            $response->write('How about implementing addBook as a POST method ?');
+            return $response;
+            });
+
+
+/**
+ * GET getBookByISBN
+ * Summary: Find book by ISBN
+ * Notes: Returns a single book
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->GET('/book/{bookISBN}', function($request, $response, $args) {
+            
+            
+            
+            
+            $response->write('How about implementing getBookByISBN as a GET method ?');
+            return $response;
+            });
+
+
+/**
+ * PUT updateBook
+ * Summary: Update an existing book
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->PUT('/book', function($request, $response, $args) {
+            
+            
+            
+            $body = $request->getParsedBody();
+            $response->write('How about implementing updateBook as a PUT method ?');
+            return $response;
+            });
+
+
+/**
+ * GET getBooks
+ * Summary: Returns a list of books
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->GET('/books', function($request, $response, $args) {
+            
+            $queryParams = $request->getQueryParams();
+            $limit = $queryParams['limit'];    $offset = $queryParams['offset'];    
+            
+            
+            $response->write('How about implementing getBooks as a GET method ?');
+            return $response;
+            });
+
+
+
+$app->run();
---
+++ .history/index_20210726084755.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Simple Bookstore API
+ * @version 1.0.0
+ */
+
+ echo 'a';die();
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Slim\App();
+
+
+/**
+ * POST addBook
+ * Summary: Add a new book to the store
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->POST('/grze.musz/LivespaceBookstore/1.0.0/book', function($request, $response, $args) {
+            
+            
+            
+            $body = $request->getParsedBody();
+            $response->write('How about implementing addBook as a POST method ?');
+            return $response;
+            });
+
+
+/**
+ * GET getBookByISBN
+ * Summary: Find book by ISBN
+ * Notes: Returns a single book
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->GET('/grze.musz/LivespaceBookstore/1.0.0/book/{bookISBN}', function($request, $response, $args) {
+            
+            
+            
+            
+            $response->write('How about implementing getBookByISBN as a GET method ?');
+            return $response;
+            });
+
+
+/**
+ * PUT updateBook
+ * Summary: Update an existing book
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->PUT('/grze.musz/LivespaceBookstore/1.0.0/book', function($request, $response, $args) {
+            
+            
+            
+            $body = $request->getParsedBody();
+            $response->write('How about implementing updateBook as a PUT method ?');
+            return $response;
+            });
+
+
+/**
+ * GET getBooks
+ * Summary: Returns a list of books
+ * Notes: 
+ * Output-Formats: [application/json, application/xml]
+ */
+$app->GET('/grze.musz/LivespaceBookstore/1.0.0/books', function($request, $response, $args) {
+            
+            $queryParams = $request->getQueryParams();
+            $limit = $queryParams['limit'];    $offset = $queryParams['offset'];    
+            
+            
+            $response->write('How about implementing getBooks as a GET method ?');
+            return $response;
+            });
+
+
+
+$app->run();
